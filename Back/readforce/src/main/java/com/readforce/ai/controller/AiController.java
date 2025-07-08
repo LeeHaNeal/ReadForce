a188f0de06f4a3be30f0b30a6cbf2d8cf764d340
Kichan-Seoul <kimkc30@icloud.com>
2025-07-08 14:31:15 +0900

    합치기 위해 커밋

--- Back/readforce/src/main/java/com/readforce/ai/controller/AiController.java
+++ Back/readforce/src/main/java/com/readforce/ai/controller/AiController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.readforce.ai.dto.AiGeneratePassageRequestDto;
+import com.readforce.ai.dto.AiGenerateTestRequestDto;
 import com.readforce.ai.service.AiService;
 import com.readforce.common.MessageCode;
 
