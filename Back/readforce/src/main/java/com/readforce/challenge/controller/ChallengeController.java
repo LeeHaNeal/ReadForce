839012ba35f47636d4c91864d52f094e0b8fa770
KimJeHyun91 <kjh910920@gmail.com>
2025-07-09 10:40:50 +0900

    수정
    - 문제 생성 오류 수정

--- Back/readforce/src/main/java/com/readforce/challenge/controller/ChallengeController.java
+++ Back/readforce/src/main/java/com/readforce/challenge/controller/ChallengeController.java
@@ -1,15 +1,19 @@
 package com.readforce.challenge.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.readforce.challenge.dto.ChallengeSubmitResultRequestDto;
 import com.readforce.challenge.service.ChallengeService;
 import com.readforce.common.MessageCode;
 import com.readforce.common.enums.CategoryEnum;
@@ -42,11 +46,13 @@ public class ChallengeController {
 		return ResponseEntity.status(HttpStatus.OK).body(resultList);
 		
 	}
-//	
+	
 //	@PostMapping("/submit-challenge-result")
-//	public ResponseEntity<Map<String, String>> submitChallengeResult(
+//	public ResponseEntity<Map<String, Double>> submitChallengeResult(
 //			@RequestBody ChallengeSubmitResultRequestDto requestDto
 //	){
+//		
+//		Double score = challengeService.get
 //		
 //		
 //		
