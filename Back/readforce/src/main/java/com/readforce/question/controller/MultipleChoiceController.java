5b987ae747f7b3e9a0eaa2b17506508320a81325
LeeHaNeal <harull817@naver.com>
2025-07-16 18:16:54 +0900

    1
    1

--- Back/readforce/src/main/java/com/readforce/question/controller/MultipleChoiceController.java
+++ Back/readforce/src/main/java/com/readforce/question/controller/MultipleChoiceController.java
@@ -14,6 +14,9 @@ import com.readforce.common.MessageCode;
 import com.readforce.question.dto.MultipleChoiceResponseDto;
 import com.readforce.question.service.MultipleChoiceService;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 
@@ -37,5 +40,14 @@ public class MultipleChoiceController {
 		return ResponseEntity.status(HttpStatus.OK).body(multipleChoiceQuestionList);
 		
 	}
-	
+	@PreAuthorize("hasRole('ADMIN')")
+	@DeleteMapping("/delete")
+	public ResponseEntity<String> deleteQuestion(
+	        @RequestParam("questionNo")
+	        @NotNull(message = "문제 번호는 필수입니다.")
+	        Long questionNo
+	) {
+	    multipleChoiceService.deleteQuestionByQuestionNo(questionNo);
+	    return ResponseEntity.ok("문제 삭제 성공");
+	}
 }
